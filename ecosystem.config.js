83b75d984630d67205285b734c116e79f8c61549
sshwy <jy.cat@qq.com>
2021-05-18 17:10:09 +0800

    build: update pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,7 @@
-const entry = process.env.production ? './app.js' : './dist/app.js';
-
-console.log(entry);
-console.log(process.env);
-
 module.exports = {
   apps: [{
     name: 'clapping-game',
-    script: entry,
+    script: './dist/app.js',
     watch: '.'
   }],
-
-  deploy: {
-    production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
-      ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
 };
