7318479fc5dd71edb5f1a8376c5325e9a5158342
sshwy <jy.cat@qq.com>
2021-05-18 12:46:53 +0800

    feature: log to file

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk');
 const logger = require('loglevel');
 const prefixer = require('loglevel-plugin-prefix');
+const fs = require("fs");
 
 const colors = {
   TRACE: chalk.magenta,
@@ -11,12 +12,25 @@ const colors = {
 };
 
 prefixer.reg(logger);
+
 logger.enableAll();
 
 prefixer.apply(logger, {
-  format(level, name, timestamp) {
+  format (level, name, timestamp) {
     return `${chalk.gray(`[${timestamp}]`)} ${colors[level.toUpperCase()](level)} ${chalk.green(`(${name}):`)}`;
   },
 })
+
+const originalFactory = logger.methodFactory;
+logger.methodFactory = function (methodName, logLevel, loggerName) {
+  var rawMethod = originalFactory(methodName, logLevel, loggerName);
+  return function (message) {
+
+    const datePrefix = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+    fs.appendFileSync(`runtime.${methodName || 'unknown'}.log`, `[${datePrefix}] (${String(loggerName)}): ${message}\n`, 'utf8');
+
+    rawMethod("Newsflash: " + message);
+  };
+};
 
 module.exports = logger;
